9ffb259a3d8fe274f4d409b4a3cdf88213fbac19
Mohammad BakiBillah Chowdhuryy (Ifty) <nblues399@gmail.com>
2021-04-15 16:22:37 +0600

    Model created. .

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, unique=True)
+    mobile_num_regex = RegexValidator(regex="^[0-9]{10,15}$", message="Entered mobile number isn't in a right format!")
+    phone_number = models.CharField(validators=[mobile_num_regex], max_length=13, blank=True)
+    bg_group = models.CharField(max_length=5)
+    sex = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class DeliveryBoy(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, unique=True)
+    mobile_num_regex = RegexValidator(regex="^[0-9]{10,15}$", message="Entered mobile number isn't in a right format!")
+    phone_number = models.CharField(validators=[mobile_num_regex], max_length=13, blank=True)
+    preferred_location = models.TextField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    brand_name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        self.total = self.product.price * self.quantity
+        return total
