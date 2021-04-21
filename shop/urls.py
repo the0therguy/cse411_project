a740aa54353bcac198576575d88a964308879ac5
Mohammad BakiBillah Chowdhuryy (Ifty) <nblues399@gmail.com>
2021-04-21 16:12:49 +0600

    Password reset view added. .

--- shop/urls.py
+++ shop/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -11,4 +12,12 @@ urlpatterns = [
     # path('change_password/', auth_views.PasswordChangeView.as_view(template_name='shop/change_password.html'), name='change_password')
     path('change_password/', views.PasswordsChangeView.as_view(template_name='shop/change_password.html')),
     path('password_success/', views.password_success, name='password_success'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='shop/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='shop/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='shop/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
